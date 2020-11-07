9f8a7a491f48fd04f38ca4da9451a2b23f3f032a
Junho <junhokimlee@gmail.com>
2020-11-07 14:17:30 -0500

    created config_maker_2win that generates config files for 2 window breps that are compatible with the new ICN_Automater_2win.gh file

---
+++ Automater_Project/src/config_maker_2win/config_maker.py
@@ -0,0 +1,74 @@
+# reads a cfg file for base and variations and outputs all necessary
+# grasshopper config files and a legend
+
+from configparser import ConfigParser
+import os
+import math
+
+# takes a line and val and returns the line with the val as the new val
+def insert_val(line,val):
+  pre = line.split(':')[0]
+  out = pre + " " + val + ","
+  return out
+
+# takes a line, list of variables, and config and returns the line with
+# updated val if the var_list var is in section list
+def rewrite(line, var_list, config, section):
+  s = line
+  l_line = line.lstrip()
+  for v in var_list:
+    v_quote = "\""+v[1]+"\""
+    l_line_s = l_line.lower()
+    if v[0] in section and l_line_s.startswith(v_quote):
+      val = config[v[0]][v[1]]
+      s = insert_val(line, val)
+  return s
+
+# find which iteration of the results we are on
+output_dir ='configs'
+s = math.ceil((sum(len(files) for _, _, files in os.walk(output_dir)))/2) +1
+
+# paths
+file = r'user_inputs\base_case.cfg'
+output_cons = output_dir + r'\cons_' + str(s) + '.txt'
+output_win = output_dir + r'\window_' + str(s) + '.txt'
+output_win2 = output_dir + r'\window2_' + str(s) + '.txt'
+default_cons = r'default_cons.txt'
+default_win = r'default_win.txt'
+
+# create config
+config = ConfigParser()
+config.read(file)
+
+# create var_list (list of variables and their sections)
+var_list = []
+for sec in config.sections():
+  for v in list(config[sec]):
+    var_list.append((sec,v))
+
+# output cons
+out_C = open(output_cons, "w")
+with open(default_cons, "r") as def_C:
+  lines = def_C.readlines()
+  for line in lines:
+    s = rewrite(line, var_list, config, ["Loads","Conditioning","Ventilation","DomHotWater","Materials"])
+    out_C.writelines(s)
+out_C.close()
+
+# output win
+out_W = open(output_win, "w")
+with open(default_win, "r") as def_W:
+  lines = def_W.readlines()
+  for line in lines:
+    s = rewrite(line, var_list, config, ["Window"])
+    out_W.writelines(s)
+out_W.close()
+
+# output win2
+out_W2 = open(output_win2, "w")
+with open(default_win, "r") as def_W:
+  lines = def_W.readlines()
+  for line in lines:
+    s = rewrite(line, var_list, config, ["Window2"])
+    out_W2.writelines(s)
+out_W2.close()
